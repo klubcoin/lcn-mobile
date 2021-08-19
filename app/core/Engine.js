cde21cd53513c49f8a69f3900f19848f2d4fb5ce
long.phan <long.phan@netpower.no>
2021-08-19 17:21:54 +0700

    Fix stuck issue with eth requests, blockTracker latest blockNumber

--- app/core/Engine.js
+++ app/core/Engine.js
@@ -68,7 +68,7 @@ class Engine {
 				}
 			);
 			const networkController = new NetworkController({
-				infuraProjectId: process.env.MM_INFURA_PROJECT_ID || NON_EMPTY,
+				infuraProjectId: AppConstants.MM_INFURA_PROJECT_ID || NON_EMPTY,
 				providerConfig: {
 					static: {
 						eth_sendTransaction: async (payload, next, end) => {
