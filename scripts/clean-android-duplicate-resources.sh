f69c4fb1dbb2eb49135a226331062d1c7cb166a0
long.phan <long.phan@netpower.no>
2021-08-19 16:26:41 +0700

    Add android dev build script

---
+++ scripts/clean-android-duplicate-resources.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd android/app/src/main/res
+rm raw/node_modules*
+rm drawable-hdpi/node_modules*
+rm drawable-mdpi/node_modules*
+rm drawable-xdpi/node_modules*
+rm drawable-xxdpi/node_modules*
+rm drawable-xxxhdpi/node_modules*
+cd -
