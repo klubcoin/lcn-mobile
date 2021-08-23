21c5b8c9aaaa2ed2161dbab3b34671ecbe9e0588
long.phan <long.phan@netpower.no>
2021-08-23 21:15:33 +0700

    Update version
    Correct paths for remove duplicate resource script

--- scripts/clean-android-duplicate-resources.sh
+++ scripts/clean-android-duplicate-resources.sh
@@ -4,7 +4,7 @@ cd android/app/src/main/res
 rm raw/node_modules*
 rm drawable-hdpi/node_modules*
 rm drawable-mdpi/node_modules*
-rm drawable-xdpi/node_modules*
-rm drawable-xxdpi/node_modules*
+rm drawable-xhdpi/node_modules*
+rm drawable-xxhdpi/node_modules*
 rm drawable-xxxhdpi/node_modules*
 cd -
