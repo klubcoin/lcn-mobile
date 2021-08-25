be443f9a48a977794a2058ec1786e727a79e70c4
long.phan <long.phan@netpower.no>
2021-08-25 15:18:43 +0700

    Implement API to proceed order via QR

--- app/services/APIService.js
+++ app/services/APIService.js
@@ -5,6 +5,7 @@ export default class APIService {
   static API_KEY = 'toto';
   static apiEtherScan = () => 'etherscan/api';
   static apiGetOrderById = () => 'pg/v1/orders/<orderId>';
+  static apiProceedOrder = () => 'pg/v1/orderpayments';
 
   static getTransactionHistory(address, callback) {
     const data = {
@@ -20,5 +21,14 @@ export default class APIService {
   static getOrderInfo(orderId, callback) {
     const route = this.apiGetOrderById().replace('<orderId>', orderId);
     WebService.sendGet(route, null, callback);
+  }
+
+  static proceedOrder(orderId, fromHash, signature, callback) {
+    const data = {
+      orderId,
+      from: fromHash,
+      sig: signature,
+    }
+    WebService.sendPost(this.apiProceedOrder(), data, callback);
   }
 }
