4c46a47843c9f3a3dba36f6de65f5be4ba45fe4c
long.phan <long.phan@netpower.no>
2021-09-01 21:12:30 +0700

    Implement preferences store

---
+++ app/store/preferences.js
@@ -0,0 +1,52 @@
+import AsyncStorage from "@react-native-community/async-storage"
+import { makeAutoObservable } from "mobx";
+
+const kAppList = 'AppList';
+
+class Preferences {
+  storage = {};
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  async load() {
+    const keys = await AsyncStorage.getAllKeys();
+    keys.map((key) => this.fetch(key));
+  }
+
+  async fetch(key) {
+    const data = await AsyncStorage.getItem(key);
+    this.storage[key] = data ? JSON.parse(data) : data;
+    return this.storage[key];
+  }
+
+  async save(key, value) {
+    this.storage[key] = value;
+    await AsyncStorage.setItem(key, JSON.stringify(value));
+  }
+
+  async saveStorage(key) {
+    await this.save(key, this.storage[key]);
+  }
+
+  async saveApp(asset) {
+    if (!this.storage[kAppList]) {
+      this.storage[kAppList] = {};
+    }
+    this.storage[kAppList][asset.address] = asset;
+    await this.saveStorage(kAppList);
+  }
+
+  async getSavedAppList() {
+    const apps = this.storage[kAppList] || {};
+    return Object.keys(apps).map(key => apps[key]);
+  }
+
+  async getAppByAddress(address) {
+    const apps = this.storage[kAppList] || {};
+    return apps[address];
+  }
+}
+
+export default new Preferences();
