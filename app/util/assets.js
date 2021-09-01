b426b957397db005ad4f786a96d296ad6ed90a57
long.phan <long.phan@netpower.no>
2021-09-01 13:44:27 +0700

    Update asset path

--- app/util/assets.js
+++ app/util/assets.js
@@ -8,7 +8,5 @@ const pack = require('../../package.json'); // eslint-disable-line
 export default function getAssetLogoPath(logo) {
 	const version = pack.dependencies['@metamask/contract-metadata']?.replace('^', '');
 	if (!logo) return;
-	const path = `https://raw.githubusercontent.com/metamask/contract-metadata/v${version}/images/`;
-	const uri = path + logo;
-	return uri;
+	return `https://docs.liquichain.io/media/app/${logo}?v=${version}`;
 }
