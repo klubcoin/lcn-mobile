2cf02558b4d11da438e683a6aae8fcea29332990
long.phan <long.phan@netpower.no>
2021-09-07 15:40:49 +0700

    Implement keycloak authentication button

---
+++ app/components/Views/LoginWithKeycloak/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { View } from 'react-native'
+import { useKeycloak } from '@react-keycloak/native';
+import StyledButton from '../../UI/StyledButton';
+
+const styles = {
+  container: {
+    marginTop: 20,
+  },
+  button: {},
+};
+
+const LoginWithKeycloak = ({ label, onSuccess, onError }) => {
+  const { keycloak } = useKeycloak();
+
+  const handleLogin = () => {
+    keycloak?.login(
+      {
+        prompt: 'login',
+        redirectUri: 'liquichain://auth',
+      }).then(() => {
+        if (onSuccess) onSuccess();
+      })
+      .catch((error) => {
+        if (onError) onError(error);
+      })
+  };
+
+  return (
+    <View style={styles.container}>
+      <StyledButton
+        style={styles.button}
+        type={'normal'}
+        onPress={handleLogin}
+      >{label || ''}</StyledButton>
+    </View>
+  );
+}
+
+export default LoginWithKeycloak;
