d73193ae7b8bf56c568ee966cd1223c3fa46741d
long.phan <long.phan@netpower.no>
2021-09-08 15:13:24 +0700

    Implement peer status announce API

--- app/services/APIService.js
+++ app/services/APIService.js
@@ -9,12 +9,21 @@ export default class APIService {
   static apiEtherScan = () => 'etherscan/api';
   static apiGetOrderById = () => 'pg/v1/orders/<orderId>';
   static apiProceedOrder = () => 'pg/v1/orderpayments';
+  static apiPeerAnnounce = () => 'announce';
 
   static routeAccountAPI = () => 'https://account.liquichain.io/meveo/api/rest/default/persistence/';
   static apiListApps = () => APIService.routeAccountAPI() + 'LiquichainApp/list';
   static apiGetAppInstances = () => APIService.routeAccountAPI() + '{cetCode}/list';
   static apiGetWalletContract = () => APIService.routeAccountAPI() + 'Wallet/{appWallet}';
 
+  static announcePeerOnlineStatus(peerId, infoHash, left, callback) {
+    const data = {
+      peer_id: peerId,
+      info_hash: infoHash,
+      left,
+    }
+    WebService.sendGet(this.apiPeerAnnounce(), data, callback);
+  }
 
   static getTransactionHistory(address, callback) {
     const data = {
