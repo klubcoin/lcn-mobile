10fc1e5b597aabe6d0da03d94f4454eeb50e20c5
long.phan <long.phan@netpower.no>
2021-09-10 02:44:15 +0700

    Add Liquichain namecard friend request

---
+++ app/components/Views/Contacts/FriendRequestMessages.js
@@ -0,0 +1,38 @@
+import routes from "../../../common/routes";
+
+export const FriendRequestTypes = ({
+  Request: 'Friend Request',
+  Accept: 'Accept Friend',
+});
+
+export const LiquichainNameCard = (selectedAddress, type) => ({
+  from: selectedAddress,
+  meta: {
+    title: routes.mainNetWork.name,
+    url: routes.mainNetWork.blockExploreUrl,
+    icon: 'logo.png',
+  },
+  data: JSON.stringify({
+    domain: {
+      chainId: routes.mainNetWork.chainId,
+      name: routes.mainNetWork.name,
+    },
+    types: {
+      EIP712Domain: [
+        { name: 'name', type: 'string' },
+        { name: 'chainId', type: 'string' },
+      ],
+      LiquichainNameCard: [
+        { name: 'type', type: 'string' },
+        { name: 'from', type: 'string' },
+        { name: 'content', type: 'string' },
+      ]
+    },
+    primaryType: 'LiquichainNameCard',
+    message: {
+      type: type || 'Name Card', // type or action for name card
+      from: selectedAddress,
+      content: `${selectedAddress} - ${routes.mainNetWork.name}`,
+    },
+  }),
+});
