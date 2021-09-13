9d1f90469a344b606c7e5018397f16e92f827e2f
long.phan <long.phan@netpower.no>
2021-09-14 02:27:15 +0700

    Add sign fixed length message method for crypto signature

--- app/core/CryptoSignature.js
+++ app/core/CryptoSignature.js
@@ -1,7 +1,14 @@
 import * as sigUtil from 'eth-sig-util';
+import * as ethUtil from 'ethereumjs-util';
 import Engine from './Engine';
 
 export default class CryptoSignature {
+	static async signMessage(from, data) {
+		const { KeyringController } = Engine.context;
+		const dataBuffer = ethUtil.bufferToHex(Buffer.from(data, 'utf8'));
+		const params = { from, data: dataBuffer };
+		return await KeyringController.signMessage(params);
+	}
 	static async signTypedData(from, typedData, version = 'V4') {
 		const { KeyringController } = Engine.context;
 		const params = { from, data: typedData };
