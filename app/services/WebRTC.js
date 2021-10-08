4999f293e83a255bbb3cfd16b1de952d09cd92c1
long.phan <long.phan@netpower.no>
2021-10-08 18:28:00 +0700

    Fix incorrect webrtc state when there are multiple connections

--- app/services/WebRTC.js
+++ app/services/WebRTC.js
@@ -8,6 +8,7 @@ import Messaging, { Message, WSEvent } from './Messaging';
 export default class WebRTC {
   fromUserId = '';
   sendChannels = {};
+  peerRefs = {};
 
   onReady = null;
   onMessage = null;
@@ -54,7 +55,7 @@ export default class WebRTC {
         switch (data.signal) {
           case 'offer': this.handleOffer(data); break;
           case 'answer': this.handleAnswer(data); break;
-          case 'ice-candidate': this.handleNewICECandidateMsg(data.candidate); break;
+          case 'ice-candidate': this.handleNewICECandidateMsg(data); break;
         }
       }
     } catch (e) { }
@@ -70,8 +71,8 @@ export default class WebRTC {
   }
 
   connectTo = (address) => {
-    this.peerRef = this.Peer(address);
-    this.sendChannels[address] = this.peerRef.createDataChannel('sendChannel');
+    this.peerRefs[address] = this.Peer(address);
+    this.sendChannels[address] = this.peerRefs[address].createDataChannel('sendChannel');
 
     // listen to incoming messages from other peer
     this.sendChannels[address].onmessage = (message) => this.handleReceiveMessage(message, address);
@@ -87,8 +88,8 @@ export default class WebRTC {
       Here we are exchanging config information
       between the peers to establish communication
     */
-    this.peerRef = this.Peer(incoming.caller);
-    this.peerRef.ondatachannel = (event) => {
+    this.peerRefs[peerId] = this.Peer(incoming.caller);
+    this.peerRefs[peerId].ondatachannel = (event) => {
       this.sendChannels[peerId] = event.channel;
       this.sendChannels[peerId].onmessage = (message) => this.handleReceiveMessage(message, peerId);
       console.log('[SUCCESS] Connection established')
@@ -104,27 +105,27 @@ export default class WebRTC {
     */
     const desc = new RTCSessionDescription(incoming.sdp);
 
-    this.peerRef.setRemoteDescription(desc).then(() => {
-      return this.peerRef.createAnswer();
+    this.peerRefs[peerId].setRemoteDescription(desc).then(() => {
+      return this.peerRefs[peerId].createAnswer();
     }).then(answer => {
-      return this.peerRef.setLocalDescription(answer);
+      return this.peerRefs[peerId].setLocalDescription(answer);
     }).then(() => {
       const payload = {
         target: incoming.caller,
         caller: this.fromUserId,
-        sdp: this.peerRef.localDescription
+        sdp: this.peerRefs[peerId].localDescription
       }
       this.sendSignal('answer', payload);
     })
   }
 
   handleAnswer = (message) => {
+    const peerId = message.caller;
     // Handle answer by the receiving peer
     const desc = new RTCSessionDescription(message.sdp);
-    this.peerRef.setRemoteDescription(desc)
+    this.peerRefs[peerId].setRemoteDescription(desc)
       .catch(err => this.onError && this.onError(err));
 
-    const peerId = message.caller;
     const sendChannel = this.sendChannels[peerId];
     // if (this.onReady) this.onReady(sendChannel);
     this.events.ready.map(callback => callback(sendChannel, peerId));
@@ -170,9 +171,10 @@ export default class WebRTC {
   }
 
   handleNewICECandidateMsg = (incoming) => {
-    const candidate = new RTCIceCandidate(incoming);
+    const peerId = incoming.caller;
+    const candidate = new RTCIceCandidate(incoming.candidate);
 
-    this.peerRef.addIceCandidate(candidate)
+    this.peerRefs[peerId].addIceCandidate(candidate)
       .catch(err => this.onError && this.onError(err));
   }
 
@@ -206,6 +208,7 @@ export default class WebRTC {
     if (e.candidate) {
       const payload = {
         target: e.target.peerId,
+        caller: this.fromUserId,
         candidate: e.candidate,
       }
       this.sendSignal('ice-candidate', payload);
@@ -214,15 +217,15 @@ export default class WebRTC {
 
   handleNegotiationNeededEvent = (peerId) => {
     // Offer made by the initiating peer to the receiving peer.
-    this.peerRef.createOffer()
+    this.peerRefs[peerId].createOffer()
       .then(offer => {
-        return this.peerRef.setLocalDescription(offer);
+        return this.peerRefs[peerId].setLocalDescription(offer);
       })
       .then(() => {
         const payload = {
           target: peerId,
           caller: this.fromUserId,
-          sdp: this.peerRef.localDescription,
+          sdp: this.peerRefs[peerId].localDescription,
         };
         this.sendSignal('offer', payload);
       })
