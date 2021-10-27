847674c3131f2cea2f2ee49ee3d41f9aff08edc4
long.phan <long.phan@netpower.no>
2021-10-28 04:53:30 +0700

    Add audio drawables

--- app/common/drawables.js
+++ app/common/drawables.js
@@ -1,4 +1,7 @@
 export default {
   avatar_profile: 'avatar_profile.png',
   avatar_user: 'avatar_user.png',
+  btnPlay: 'btn_play.png',
+  btnPause: 'btn_pause.png',
+  btnReplay: 'btn_replay.png'
 }
