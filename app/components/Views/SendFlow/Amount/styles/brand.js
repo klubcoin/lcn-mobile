f5eb7defd1719c85fd03ee43bfc676c3f111628a
TaiTrien <v.v.t.trien@gmail.com>
2021-11-02 13:04:32 +0700

    feat: update theme for Amount

---
+++ app/components/Views/SendFlow/Amount/styles/brand.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+import { colors, fontStyles } from '../../../../../styles/common';
+import Device from '../../../../../util/Device';
+
+const brandStyles = StyleSheet.create({
+	root: {
+		backgroundColor: colors.transparent
+	},
+	wrapper: {
+		backgroundColor: colors.transparent
+	}
+});
+
+export { brandStyles };
