87b455695cc73697c70046811568b5d23bb6a79d
long.phan <long.phan@netpower.no>
2021-11-06 01:02:43 +0700

    Implement market app details page

---
+++ app/components/Views/MarketPlace/Application/test.js
@@ -0,0 +1,29 @@
+export default {
+  desc: 'A cryptocurrency is a digital or virtual currency that is secured by cryptography, which makes it nearly impossible to counterfeit or double-spend. Many cryptocurrencies are decentralized networks based on blockchain technology—a distributed ledger enforced by a disparate network of computers. A defining feature of cryptocurrencies is that they are generally not issued by any central authority, rendering them theoretically immune to government interference or manipulation. Crypto-friendly banking: Multi-currency payment accounts (supporting 25+ currencies), with the option of linking virtual IBANs, enable crypto businesses to extend their value chain and provide collection, conversion and settlement services to customers. \
+\nSimplified cryptocurrency purchase: Customers can purchase digital currencies instantly and securely using their preferred debit and credit card or bank wire transfer.\
+\nFiat and digital currency exchange: OpenPayd enablesthe seamless exchange to and from fiat and cryptocurrencies.\
+\nAutomate payments: Integrate the OpenPayd platform into existing finance and treasury management systems to simplify payment flow management.',
+  banner: 'https://images.ctfassets.net/6ndujxh3wgyb/4JHqblTjObqOS6ZyvnIgZR/befa6771d79f676346ef7edea9e2d02c/7th-July_crypto-post.jpg',
+  reviews: [
+    {
+      name: 'John Henry',
+      comment: 'A cryptocurrency is a digital or virtual currency that is secured by cryptography, which makes it nearly impossible to counterfeit or double-spend. Many cryptocurrencies are decentralized ',
+      rating: 4.6
+    },
+    {
+      name: 'Tom Carter',
+      comment: 'A cryptocurrency is a digital or virtual currency that is secured by cryptography',
+      rating: 4.7
+    },
+    {
+      name: 'Mark Hill',
+      comment: 'A cryptocurrency is a digital or virtual currency that is ',
+      rating: 4.8
+    },
+    {
+      name: 'Terry Boros',
+      comment: 'A cryptocurrency is a digital or virtual currency that is secured ',
+      rating: 4.2
+    }
+  ]
+}
