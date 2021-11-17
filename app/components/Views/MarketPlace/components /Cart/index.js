ae4afe13e9eebc8b8168b84eb4c23eacc1340f0a
long.phan <long.phan@netpower.no>
2021-11-18 03:06:20 +0700

    Implement custom cart widget

---
+++ app/components/Views/MarketPlace/components /Cart/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { inject, observer } from 'mobx-react';
+import IonIcon from 'react-native-vector-icons/Ionicons';
+import { RFValue } from 'react-native-responsive-fontsize';
+import styles from './styles';
+
+const Cart = ({ market, color, onClick }) => {
+	return (
+		<View style={styles.cart}>
+			<TouchableOpacity onPress={() => onClick && onClick()} style={styles.button}>
+				<IonIcon style={styles.cartIcon} name={'cart-outline'} size={RFValue(18)} color={color} />
+				{market.cartBadge > 0 && (
+					<View style={styles.badge}>
+						<Text style={styles.counter}>{market.cartBadge}</Text>
+					</View>
+				)}
+			</TouchableOpacity>
+		</View>
+	)
+}
+
+export default inject('market')(observer(Cart));
