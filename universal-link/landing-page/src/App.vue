fd868f70a2e780a38e673f34f8906b1da126ab74
long.phan <long.phan@netpower.no>
2022-01-01 02:21:26 +0700

    Implement vuejs landing page

---
+++ universal-link/landing-page/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+	<div>
+		<ImageLink
+			class="site-logo"
+			href="https://www.liquichain.io/"
+			source="https://www.liquichain.io/assets/images/logo-white.svg"
+		/>
+		<h1>A sustainable blockchain for the people.</h1>
+		<div class="app-links">
+			<ImageLink
+				href="https://play.google.com/store/apps/details?id=com.liquichain"
+				:source="require('@/assets/google_play.png')"
+			/>
+			<ImageLink
+				href="https://apps.apple.com/us/app/treadmeal/id1581708293"
+				:source="require('@/assets/apple_store.png')"
+			/>
+		</div>
+		<Content />
+	</div>
+</template>
+
+<script>
+import ImageLink from './components/ImageLink.vue';
+import Content from './components/Content.vue';
+
+export default {
+	name: 'App',
+	components: {
+		ImageLink,
+		Content,
+	},
+};
+</script>
+
+<style>
+body {
+	display: flex;
+	flex: 1;
+	background: #310a6b;
+	color: #fff;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+}
+
+#app {
+	display: flex;
+	flex: 1;
+	flex-direction: column;
+	text-align: center;
+}
+
+.site-logo img {
+	width: 250px;
+}
+
+.app-links {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+.image-link:last-child {
+	margin-left: 20px;
+}
+</style>
---
+++ universal-link/landing-page/src/components/Content.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="content-wrapper"></div>
+</template>
+
+<script>
+export default {
+	name: 'Content',
+	props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.content-wrapper {
+	margin-top: 40px;
+}
+</style>
