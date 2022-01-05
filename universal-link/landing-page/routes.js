8d78c3f898e1a46e2c7b5fabb5809e84e647a797
TaiTrien <v.v.t.trien@gmail.com>
2022-01-05 16:07:21 +0700

    feat: add username and refactor code for Vue

---
+++ universal-link/landing-page/routes.js
@@ -0,0 +1,31 @@
+export default {
+  jsonRPC: 'jsonrpc',
+  walletCreation: 'wallet_creation',
+  walletUpdate: 'wallet_update',
+  walletInfo: 'wallet_info',
+  walletReport: 'wallet_report',
+  getBalance: 'eth_getBalance',
+  getTransaction: 'eth_getTransactionByHash',
+  mainNetWork: {
+    name: 'Liquichain Main Network',
+    url: 'https://account.liquichain.io/meveo/rest/jsonrpc',
+    chainId: '76',
+    symbol: null,
+    ticker: 'LCN',
+    coin: 'Licoin',
+    route: 'https://account.liquichain.io/meveo/rest/',
+    hostDomain: 'liquichain.io',
+    blockExploreUrl: 'https://liquichain.io',
+    accountUrl: 'https://account.liquichain.io',
+    helpSupportUrl: 'https://docs.liquichain.io',
+    reportIssueUrl: 'https://github.com/liquichain/lcn-mobile/issues',
+  },
+  basicMethod: {
+    "jsonrpc": "2.0",
+    "method": null,
+    "params": null // [mywallet, publicAddress, publicKey]
+  },
+  getConversions: 'currconv',
+  paypalCreateOrder: "payment",
+  paypalPaymentCapture: "payment-capture"
+}
--- universal-link/landing-page/src/components/Content.vue
+++ universal-link/landing-page/src/components/Content.vue
@@ -1,7 +1,8 @@
 <template>
-	<div class="content-wrapper">
-		<h1>Tip {{tipValue}} {{symbol}} for Neirt Vo</h1>
-		<QRCode :tipData="qrValue" />
+	<div class="content-wrapper" align="center">
+		<h1>Tip {{ tipValue }} {{ symbol }} for {{ recipient?.name || 'Anonymous' }}</h1>
+		<p v-on:click="changeAddressType" class="address">{{ renderAddress() }}</p>
+		<QRCode :tipData="tipLink" />
 	</div>
 </template>
 
@@ -9,36 +10,79 @@
 import QRCode from './QRCode.vue';
 import qs from 'qs';
 import { fromWei } from 'web3-utils';
+import routes from '../../routes';
 
 const host = 'http://meetthrufriends.com';
 var tipLink = '';
 var tipValue = 0;
 var symbol = '';
-
-function initQRValue() {
-	try {
-		var recipientAddress = window.location.pathname.split('/')[2];
-		var queryString = window.location.search.replace('?', '');
-		var params = qs.parse(queryString);
-
-		tipValue = fromWei(params.value, 'ether');
-		symbol = params.symbol;
-		tipLink = `${host}/tip/${recipientAddress}?value=${params.value}&symbol=${params.symbol}&isETH=${params.isETH}&decimals=${params.decimals}`;
-	} catch (error) {
-		console.log('error', error);
-	}
-}
+var recipient = {};
+var addressType = 'short';
 
 export default {
 	name: 'Content',
 	data() {
 		return {
-			qrValue: tipLink,
-			tipValue: tipValue,
-			symbol: symbol,
+			tipLink,
+			tipValue,
+			symbol,
+			recipient,
+			addressType
 		};
 	},
-	beforeCreate: initQRValue(),
+	methods: {
+		async fetchWalletInfo() {
+			const url = routes.mainNetWork.url;
+			const requestOpts = {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({
+					...routes.basicMethod,
+					method: routes.walletInfo,
+					params: [this.recipient.address]
+				})
+			};
+			try {
+				const response = await fetch(url, requestOpts);
+				const data = await response.json();
+				if (data?.result) {
+					this.recipient.name = data.result.name;
+				}
+			} catch (error) {
+				console.log('error', error);
+			}
+		},
+		initData() {
+			try {
+				var recipientAddress = window.location.pathname.split('/')[2];
+				var queryString = window.location.search.replace('?', '');
+				var params = qs.parse(queryString);
+
+				this.tipValue = fromWei(params.value, 'ether');
+				this.symbol = params.symbol;
+				this.recipient.address = recipientAddress;
+
+				this.tipLink = `${host}/tip/${recipientAddress}?value=${params.value}&symbol=${params.symbol}&isETH=${
+					params.isETH
+				}&decimals=${params.decimals}`;
+				console.log(this.tipLink);
+				this.fetchWalletInfo();
+			} catch (error) {
+				console.log('error', error);
+			}
+		},
+		renderAddress() {
+			if (this.addressType == 'short')
+				return `${recipient.address.substr(0, 4 + 2)}...${recipient.address.substr(-4)}`;
+			else return recipient.address;
+		},
+		changeAddressType() {
+			this.addressType == 'short' ? (this.addressType = 'full') : (this.addressType = 'short');
+		}
+	},
+	created() {
+		this.initData();
+	},
 	components: {
 		QRCode
 	}
@@ -52,5 +96,8 @@ export default {
 	margin-bottom: 40px;
 	flex-direction: column;
 	flex: 1;
+}
+.address {
+	width: fit-content;
 }
 </style>
