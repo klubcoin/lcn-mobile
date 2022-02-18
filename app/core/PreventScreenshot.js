d6594389b2fd4febb84b002a3036ba4b50f387d8
TaiTrien <v.v.t.trien@gmail.com>
2022-02-18 09:16:44 +0700

    unlock prevent screenshot temporarily

--- app/core/PreventScreenshot.js
+++ app/core/PreventScreenshot.js
@@ -3,6 +3,6 @@ import { NativeModules, Platform } from 'react-native';
 const isAndroid = Platform.OS === 'android';
 
 export default {
-	forbid: isAndroid ? NativeModules.PreventScreenshot.forbid : () => true,
+	forbid: isAndroid ? NativeModules.PreventScreenshot.allow : () => true,
 	allow: isAndroid ? NativeModules.PreventScreenshot.allow : () => true
 };
