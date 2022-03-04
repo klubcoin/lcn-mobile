aa720c4bea95762e0dbc4de13909ff4e30a93d9d
hanle9 <58067361+hanle9@users.noreply.github.com>
2022-03-04 18:07:01 +0700

    feat: Fix bug authentication when call api

--- app/variants/brands/Dev.js
+++ app/variants/brands/Dev.js
@@ -1,6 +1,6 @@
 class Dev {
 	static ServerAddress = 'https://account2.liquichain.io/';
-    static basicAuth = 'apitest/apitest';
+    static basicAuth = 'apitest:apitest';
     static  keycloakUrl='https://account2.liquichain.io/auth';
     static universalLink='account2.liquichain.io'
 }
